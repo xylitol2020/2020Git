e7314d7e545037929fcf1733bbde32438ff549f6
xylitol2020 <1405808867@qq.com>
2020-06-09 15:39:59 +0800

    提交到本地仓库

---
+++ eclipseGit/src/com/xylitol/dao/Student.java
@@ -0,0 +1,17 @@
+package com.xylitol.dao;
+
+public class Student {
+	private int id;
+	private String username;
+	private String password;
+	private int sex;
+	private String love;
+	
+	public void study() {
+		
+	}
+	public void eat() {
+		
+	}
+
+}
